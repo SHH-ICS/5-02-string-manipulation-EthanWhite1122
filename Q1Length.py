2a327fd4dc8b81abbed433832e0fb7290d157fb2
Ethan White <ethan.white1@stu.ocsb.ca>
2024-11-27 09:55:56 -0500

    Update Q1Length.py

--- Q1Length.py
+++ Q1Length.py
@@ -1,2 +1,9 @@
 # Create a program that will accept a word from a user and return the length of that word. 
 # Make this program in a loop with option to exit when the use types in quit.
+w=str(input("Please enter a word:"))
+try: 
+    x=int(w)
+    print("Enter a word please")
+except ValueError:
+    l=len(w)
+    print("The word" , w , "is" , l  ,"letters long!")
