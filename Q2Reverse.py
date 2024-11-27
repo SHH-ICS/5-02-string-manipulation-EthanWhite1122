15eb359734a4c7d691cbdbef27e8a790451e974d
Ethan White <ethan.white1@stu.ocsb.ca>
2024-11-27 09:56:15 -0500

    Update Q2Reverse.py

--- Q2Reverse.py
+++ Q2Reverse.py
@@ -1,1 +1,6 @@
 # Create a program that will accept a word and output the word one letter at a time in reverse.
+w=str(input("Please enter a word:"))
+x=len(w)
+for letter in reversed(w):
+    print(letter)
+print("    ")
